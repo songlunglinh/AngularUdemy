359f6ba393957be04786e54a57520be9a8f82b66
songlunglinh <dobken0702@gmail.com>
2017-01-21 14:13:17 +0700

    add example4

---
+++ example4/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exampleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the exampleApp
+ */
+angular.module('exampleApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ example4/app/scripts/controllers/myroute.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exampleApp.controller:MyrouteCtrl
+ * @description
+ * # MyrouteCtrl
+ * Controller of the exampleApp
+ */
+angular.module('exampleApp')
+  .controller('MyrouteCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
